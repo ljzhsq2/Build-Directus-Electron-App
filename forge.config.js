84cf716f33398654a32e05d995867f89d423d9b0
- <->
2025-11-10 19:11:41 +0800

    主要修改:
      1. 移除了所有图标相关配置 - 删除了 icon、setupIcon、iconUrl、loadingGif 等配置
      2. 移除了 Linux 构建配置 - 删除了 maker-deb 和 maker-rpm，因为只需要 Windows 构建
      3. 简化了配置 - 只保留必要的 Squirrel (Windows 安装程序) 和 ZIP 配置
    
      现在构建会使用 Electron 的默认图标,不会再报图标错误了。
    
      你可以:
      1. 推送这个修改:
      git add forge.config.js
      git commit -m "Fix icon error in build config"
      git push origin main
    
      2. 重新打 tag 触发构建:
      git tag -d v11.5.1
      git push --delete origin v11.5.1
      git tag -a v11.5.1 -m "Release v11.5.1"
      git push origin v11.5.1
    
      如果以后想要自定义图标,只需要在 resources/ 目录下添加 icon.ico
      文件,然后在配置中重新启用图标设置即可。

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './resources/icon',
     protocols: [
       {
         name: 'Directus App',
@@ -18,34 +17,11 @@ module.exports = {
         name: 'DirectusApp',
         authors: 'Your Name',
         description: 'Directus 11.5.1 Offline Desktop Application',
-        iconUrl: 'https://raw.githubusercontent.com/yourusername/yourrepo/main/resources/icon.ico',
-        setupIcon: './resources/icon.ico',
-        loadingGif: './resources/loading.gif',
       },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'win32'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {
-        options: {
-          maintainer: 'Your Name',
-          homepage: 'https://github.com/yourusername/yourrepo',
-          icon: './resources/icon.png',
-        },
-        mimeType: ['x-scheme-handler/directus'],
-      },
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        options: {
-          homepage: 'https://github.com/yourusername/yourrepo',
-          icon: './resources/icon.png',
-        },
-      },
+      platforms: ['win32'],
     },
   ],
 };
